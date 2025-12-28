2cf1a4110b9d69bdaf6b986a6e1a7cbbd8df8fdd
barbara <laqrize@gmail.com>
2025-12-28 18:22:39 +0100

    feat: Add CommonsAutoConfiguration for component scanning of the 'commons' package

---
+++ src/main/java/pl/ros/commons/config/CommonsAutoConfiguration.java
@@ -0,0 +1,9 @@
+package pl.ros.commons.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pl.ros.commons")
+public class CommonsAutoConfiguration {
+}
